dd084abcb0b429b9bddc3205486ed62ce51ec764
KirimaK <kirima111.04@gmail.com>
2025-08-16 14:39:12 +0700

    Hands-on Lab: Writing Your First Javascript Function

--- JavaScript Programming Essentials/calculateArea/calculate_Area.js
+++ JavaScript Programming Essentials/calculateArea/calculate_Area.js
@@ -8,3 +8,13 @@ function calculateArea() {
     let area = length * width;
     document.getElementById("result").innerHTML = `The area of the rectangle is: ${area}`;
 }
+
+function calculateGroceryTotal() {
+    let item1 = parseFloat(document.getElementById("grocery1").value);
+    let item2 = parseFloat(document.getElementById("grocery2").value);
+    let item3 = parseFloat(document.getElementById("grocery3").value);
+
+    let total = item1 + item2 + item3;
+
+    document.getElementById("groceryResult").innerHTML = `The total cost of groceries is: $${total.toFixed(2)}`;
+}
